b0bcf8baabc15bd86b24b04573d7428037298e99
uly <ulyfleen@gmail.com>
2022-04-19 18:30:11 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int max = GetInput("Enter the maximum number of the range");//prompts user for max number
+        for (int i = 0; i < max; i++) {//i is the number to check
+            if (isPrime(i)) { //if the number is prime
+                System.out.println(i); //print it
+            }
+
+        }
+
+
+    }
+    public static int GetInput(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        return scanner.nextInt();
+    }
+
+    public static boolean isPrime(int num){
+        if (num == 1){
+            return false; // 1 is not prime
+        }
+        for (int i = 2; i < num; i++){ //i is the divisor
+            if (num % i == 0){ //if the number is divisible by i
+                return false; //it is not prime
+            }
+        }
+        return true;
+    }
+}
